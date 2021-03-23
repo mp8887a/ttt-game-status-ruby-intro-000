fd0403350f4f802319e8c4670b8c1f3284b433c2
Mike Pokalsky <mpoksky1989@gmail.com>
2021-03-23 19:21:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,6 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    elsif position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return false
     else
       false
     end
